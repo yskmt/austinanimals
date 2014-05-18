81e844a7885fce4319fac95d797c167afa6579c2
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-05-17 19:49:04 -0500

    connection to gmap

--- austinanimals.py
+++ austinanimals.py
@@ -1,7 +1,9 @@
 import tweepy
-
+from geopy import geocoders
 from bs4 import BeautifulSoup
 from urllib2 import urlopen, Request
+
+import json
 
 import re
 
@@ -12,6 +14,127 @@ consumer_secret = "dwRK6TtYsTAALzOSBWhmpujC4eoeVzqYxzfono0irO8B8bvwzL"
 
 access_token = "19383289-bSwFXjI56RLY0HD4fNmHB1TH48Zy32VWwkN8mcbsl"
 access_token_secret = "WbXrw6fwI7tADxmLifd2HMFSb7r29QW3Dk9z39HCF2SWH"
+
+
+def make_soup(url):
+    # need to set user-agent
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(url,headers=hdr)
+    page = urlopen(req)
+
+    return BeautifulSoup(page, "lxml")
+
+# empty object to hold pet information
+class petClass:
+    def __init__(self):
+        self.addr = None
+        self.lat = None
+        self.link = None
+        self.lng = None
+        self.petID = None
+        self.pet_type = None
+        self.petinfo = None
+        self.place = None
+
+
+def geocode(g, addr):
+
+    # try Texas
+    results = g.geocode(addr+' Texas', exactly_one=False)
+
+    for place, (lat, lng) in results:
+        if re.search(r'(Austin|Lakeway|Llano|Del Valle|Manor|Driftwood)', place):
+            # print "%s: %.5f, %.5f" % (place, lat, lng)  
+            return place, (lat, lng)
+
+    for place, (lat, lng) in results:
+        if re.search(r'TX', place):
+            return place, (lat, lng)
+
+    # try Austin TX
+    results = g.geocode(addr+' Austin, Texas', exactly_one=False)
+
+    for place, (lat, lng) in results:
+        if re.search(r'(Austin|Lakeway|Llano|Del Valle|Manor|Driftwood)', place):
+            # print "%s: %.5f, %.5f" % (place, lat, lng)  
+            return place, (lat, lng)
+
+    for place, (lat, lng) in results:
+        if re.search(r'TX', place):
+            return place, (lat, lng)
+
+
+    return results[0]
+
+def get_pet_info(url):
+    'Obtain pet information and return the list of petClass objects'
+
+    soup = make_soup(url)
+
+    textbody = soup.find("div", id="text")
+
+    if textbody==None:
+        # url_redirect = re.search(r'url=(.+)\"\s',str(soup.find("meta", attrs={'content': re.compile('url')}))).group(1)
+
+        url_redirect = re.search(r'url=(.+)\"\s', \
+                                 str(soup.find("meta", attrs={'http-equiv': re.compile(r'(r|R)efresh')}))).group(1)
+
+        print "url_redirect = " + url_redirect
+
+        soup = make_soup(url_redirect)
+        textbody = soup.find("div", id="text")
+
+
+    # print textbody
+    text_contents = textbody.contents
+
+    # geocoding using Google V3 API
+    g = geocoders.GoogleV3(api_key='AIzaSyB7LvwvLJN0l04rFfHbIyUBsqi61vP6qWA')
+
+    pets = []
+    for i in range(len(text_contents)/2):
+        petregex = re.search(r'(\d+\))(.+):.*(DOG|CAT|BIRD)(.*)', text_contents[i*2])
+
+        if petregex:
+            pet = petClass() 
+
+            petID = petregex.group(2).replace(' ','')
+            pettype = petregex.group(3).replace(' ','')
+            petdesc = petregex.group(4).split(',')
+            addr = ''
+
+            petinfo = []
+            for j in range(len(petdesc)):
+                if (len(petdesc[j]) != 0) or (re.match('\s+',petdesc[j]) != None):
+                    addr_re = re.search(r'found at (.*)',petdesc[j])
+                    if addr_re:
+                        addr = addr_re.group(1)
+                        pet.addr = addr.replace('/', ' and ')
+
+                        place, (lat, lng) = geocode(g, pet.addr)  
+                        pet.place = place
+                        pet.lat = lat
+                        pet.lng = lng
+                    else:
+                        petinfo.append(petdesc[j].strip())
+
+            link =  text_contents[i*2+1].string
+
+            if any([a==None for a in [petID, pettype, petdesc]]):
+                print petregex
+
+            pet.petID = petID
+            pet.pet_type = pettype
+            pet.petinfo = petinfo
+            pet.link = link
+
+            pets.append(pet)
+
+        else:
+            print "regex failed!"
+
+    return pets
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -30,94 +153,29 @@ for i in range(len(austinanimals_timeline)):
     if search_result:
         tinyuri_urls.append(search_result.group(1))
 
-
-
-def make_soup(url):
-    # need to set user-agent
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(url,headers=hdr)
-    page = urlopen(req)
-
-    return BeautifulSoup(page, "lxml")
-
-# empty object to hold pet information
-class petClass:
-    pass
-
-def get_pet_info(url):
-    'Obtain pet information and return the list of petClass objects'
-
-    soup = make_soup(url)
-
-    textbody = soup.find("div", id="text")
-
-    if textbody==None:
-        # url_redirect = re.search(r'url=(.+)\"\s',str(soup.find("meta", attrs={'content': re.compile('url')}))).group(1)
-
-        pdb.set_trace()
-        url_redirect = re.search(r'url=(.+)\"\s', \
-                                 str(soup.find("meta", attrs={'http-equiv': re.compile(r'(r|R)efresh')}))).group(1)
-
-        print "url_redirect = " + url_redirect
-
-        soup = make_soup(url_redirect)
-
-    # pdb.set_trace()
-    # print textbody
-    text_contents = textbody.contents
-
-    pets = []
-    for i in range(len(text_contents)/2):
-        petregex = re.search(r'(\d+\))(.+):.*(CAT|DOG)(.*)', text_contents[i*2])
-        petID = petregex.group(2)
-        pettype = petregex.group(3)
-        petdesc = petregex.group(4).split(',')
-        addr = 'N/A'
-
-        petinfo = []
-        for j in range(len(petdesc)):
-            if (len(petdesc[j]) != 0) or (re.match('\s+',petdesc[j]) != None):
-                addr_re = re.search(r'found at (.*)',petdesc[j])
-                if addr_re:
-                    addr = addr_re.group(1)
-                else:
-                    petinfo.append(petdesc[j])
-
-        link =  text_contents[i*2+1].string
-
-        pet = petClass() 
-        pet.petID = petID
-        pet.pet_type = pettype
-        pet.addr = addr.replace('/', ' and ') + ' TX' # +'Austin'
-        pet.petinfo = petinfo
-        pet.link = link
-
-        # geocoding using Google V3 API
-        g = geocoders.GoogleV3(api_key='AIzaSyB7LvwvLJN0l04rFfHbIyUBsqi61vP6qWA')
-        place, (lat, lng) = g.geocode(pet.addr)  
-
-        pet.place = place
-        pet.lat = lat
-        pet.lng = lng
-
-
-
-        pets.append(pet)
-
-    return pets
-
-
 A = []
 for i in range(len(tinyuri_urls)):
     A += get_pet_info(tinyuri_urls[i])
 
 for pt in A:
-    print pt.petID
-    print pt.pet_type
-    print pt.addr
-    print pt.petinfo
-    print pt.link 
+    # print pt.petID
+    # print pt.pet_type
+    # print pt.addr
+    # print pt.petinfo
+    # print pt.link 
     print pt.place
-    print pt.lat 
-    print pt.lng
+    # print pt.lat 
+    # print pt.lng
+
+
+with open('pets.js', 'w') as f:
+    f.write('var pet_arr = [];\n')
+    for i in range(len(A)):
+        f.write('pet_arr.push({0:s});\n'.format(json.dumps(A[i].__dict__)))
+
+
+
+# # geocoding using Google V3 API
+# g = geocoders.GoogleV3(api_key='AIzaSyB7LvwvLJN0l04rFfHbIyUBsqi61vP6qWA')
+
 
---
+++ gmap_script.js
@@ -0,0 +1,108 @@
+var map;
+var currentPosition;
+
+// // get the current address
+// if (navigator.geolocation) {
+// navigator.geolocation.getCurrentPosition(function(position) {
+// currentPosition = position;
+// /* map.setCenter(new google.maps.LatLng(position.coords.latitude, */
+// /* position.coords.longitude)) */
+// }, function() {
+// // alert('We cannot get your current location!!');
+// });
+// } else {
+// // alert('Your browser is not compatible with geolocation!!');
+// }
+
+// geocoding
+var geocoder = new google.maps.Geocoder();
+geocoder.geocode({
+	'address' : 'Austin, TX'
+}, showMainMap);
+
+function moveMap() {
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({
+		'address' : document.getElementById('address').value
+	}, function(result, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.panTo(result[0].geometry.location);
+		} else {
+			alert("Address Geocoding ERROR!!");
+		}
+	});
+}
+
+function showMainMap(result, status) {
+	if (status == google.maps.GeocoderStatus.OK) {
+		var latlng;
+		if (currentPosition !== undefined) {
+			latlng = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
+		} else {
+			latlng = result[0].geometry.location;
+		}
+
+		var options = {
+			zoom : 12,
+			center : latlng,
+			mapTypeId : google.maps.MapTypeId.ROADMAP,
+			panControl : true,
+			zoomControl : true,
+			scaleControl : true
+		};
+
+		// define map
+		map = new google.maps.Map(document.getElementById('map'), options);
+
+		// add markers on click
+		// addMinionMarker(map);
+	} else {
+		alert('Error Geocoding!');
+	}
+
+}
+
+function addPetMarker(map, pet) {
+
+	var trailerLatLng = new google.maps.LatLng(pet.lat, pet.lng);
+
+	if (pet.pet_type === 'DOG'){
+		icon_url = 'dog.png';
+		// http://findicons.com/icon/38862/dog
+	}
+	else{
+		icon_url = 'cat.png';
+		// http://findicons.com/icon/38863/cat
+	}
+
+	var marker = new google.maps.Marker({
+		position : trailerLatLng,
+		map : map,
+		icon : {
+			url : icon_url,
+			scaledSize : new google.maps.Size(40, 45)
+		},
+		title : pet.petID,
+		draggable : false
+	});
+	// map.panTo(trailerLatLng);
+	markersArray.push(marker);
+
+	petInfoWindow(map, marker, pet, trailerLatLng);
+
+}
+
+function petInfoWindow(map, marker, pet, trailerLatLng) {
+
+	var infoWindow = new google.maps.InfoWindow({
+		content : pet.petinfo.join(" "),
+		position : map.getCenter(),
+		maxWidth: 320
+	});
+	google.maps.event.addListener(marker, 'click', function() {
+		infoWindow.open(map, marker);
+	});
+
+	console.log(pet.petinfo.join(" "));
+
+}
